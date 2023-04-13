19a5d40a9ca5cf309837863106af9fe6efba16b3
Rafael Cavalcante <rafacavalcante081@gmail.com>
2023-04-13 15:40:16 -0300

    router is working

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,28 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { HomeComponent } from './views/home/home.component';
+import { ProjectsComponent } from'./views/projects/projects.component';
+import { ExperienceComponent }  from './views/experience/experience.component';
+import { SkillsComponent } from './views/skills/skills.component';
+
+
+
+const routes: Routes = [
+  {
+    path: "",
+    component: HomeComponent
+  },{
+    path: "projects",
+    component: ProjectsComponent
+  },{
+    path: "experiences",
+    component: ExperienceComponent
+  }, {
+    path: "skills",
+    component: SkillsComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,20 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/template/header/header.component';
 import { FooterComponent } from './components/template/footer/footer.component';
+import { HomeComponent } from './views/home/home.component';
+import { ProjectsComponent } from './views/projects/projects.component';
+import { ExperienceComponent } from './views/experience/experience.component';
+import { SkillsComponent } from './views/skills/skills.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
+    HomeComponent,
+    ProjectsComponent,
+    ExperienceComponent,
+    SkillsComponent,
   ],
   imports: [
     BrowserModule,
